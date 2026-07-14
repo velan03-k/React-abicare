72ee6a94ef57beb9fc63a827ca3ebc3b63aabf48
velan03-k <velavank2000@gmail.com>
2026-07-14 23:01:40 +0530

    Login and Signup uses Local Storage

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import {BrowserRouter} from 'react-router-dom';
 import AppRouter from './Routes/AppRouter';
 import Navbar from './Components/Navbar';
 import Footer from './Components/Footer';
-import AuthLayout from './Components/AuthLayout';
+// import AuthLayout from './Components/AuthLayout';
 import { useLocation } from 'react-router-dom';
 
 
