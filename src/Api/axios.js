d6a51f7d93cc1c9e8b069787afd70a162d7163eb
velan03-k <velavank2000@gmail.com>
2026-07-22 22:58:31 +0530

    Update and Delete funtions

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://backend-hvvh.onrender.com",
+  baseURL: "https://backend-hvvh.onrender.com/api",
 });
 
 API.interceptors.request.use((config) => {
